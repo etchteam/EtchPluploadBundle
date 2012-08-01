e0704db769ce7deb97191cb2f8f1f96dceb4ec5c
Kevin Dew <kev@redbullet.co.uk>
2012-08-01 19:05:33 +0100

    Basic Bundle

---
+++ EtchPluploadBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Etch\PluploadBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Etch Plupload Bundle
+ *
+ * @author Kevin Dew <kev@etchapps.com>
+ *
+ */
+class EtchPluploadBundle extends Bundle
+{
+}
